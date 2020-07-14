6268c1e56dd1548a0e9bdcc8901e085eca215e03
Leonardo <leosud099@gmail.com>
2020-07-13 18:00:45 -0600

    Playing with Node

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-import './config';
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.write('Hello HTTP!\n');
+    setTimeout(() => {
+        res.write('I can stream!\n');
+        res.end();
+    }, 3000);
+});
+
+server.listen(8080);
